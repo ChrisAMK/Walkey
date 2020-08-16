adf344bed18226bf9cab43c5c70e72319acc16da
Nick  Sword <nicksword@hotmail.com>
2020-08-16 11:07:28 +0800

    Added basic boilerplate code

---
+++ config/config.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+    development: {
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: "walke",
+      host: "127.0.0.1",
+      port: 3306,
+      dialect: "mysql"
+    },
+    test: {
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: "walke",
+      host: "127.0.0.1",
+      port: 3306,
+      dialect: "mysql"
+    },
+    production: {
+      // eslint-disable-next-line camelcase
+      // eslint-disable-next-line camelcase
+      use_env_variable: "JAWSDB_URL",
+      dialect: "mysql"
+    }
+  };
