d76cec84f9ffea0edd40e70f9501cd51384e9910
Nick  Sword <nicksword@hotmail.com>
2020-08-18 19:14:43 +0800

    Sequelize connection working in server

--- db/schema.sql
+++ db/schema.sql
@@ -2,16 +2,16 @@ DROP DATABASE IF EXISTS walke;
 
 CREATE DATABASE walke;
 
-USE walke;
+-- USE walke;
 
-CREATE TABLE users (
-	id INT NOT NULL auto_increment,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    dob date NOT NULL,
-    primary key (id)
-);
+-- CREATE TABLE users (
+-- 	id INT NOT NULL auto_increment,
+--     name VARCHAR(255) NOT NULL,
+--     email VARCHAR(255) NOT NULL,
+--     password VARCHAR(255) NOT NULL,
+--     dob date NOT NULL,
+--     primary key (id)
+-- );
 
 
-SELECT * FROM users;
+-- SELECT * FROM users;
--- server.js
+++ server.js
@@ -1,33 +1,31 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config();
+// }
 
-// const routes = require("./controllers/routes.js");
-// const connection = require("./config/connection.js");
-const express = require('express');
-const server = express();
-const passport = require('passport');
-const flash = require('express-flash');
-const session = require('express-session');
-const methodOverride = require('method-override');
-const initializePassport = require('./passport-config');
+// const express = require('express');
+// const server = express();
+// const passport = require('passport');
+// const flash = require('express-flash');
+// const session = require('express-session');
+// const methodOverride = require('method-override');
+// const initializePassport = require('./passport-config');
+// const db = require("./models");
+// /// Grabing all the users and passing them into the initialize Passport Function
+// function updateUserListForPassport() {
+//   var queryString = "SELECT * FROM users;";
+//   connection.query(queryString, function (err, result) {
+//     if (err) throw err;
 
-/// Grabing all the users and passing them into the initialize Passport Function
-function updateUserListForPassport() {
-  var queryString = "SELECT * FROM users;";
-  connection.query(queryString, function (err, result) {
-    if (err) throw err;
-
-    initializePassport(
-      passport,
-      email => result.find(user => user.email === email),
-      id => result.find(user => user.id === id)
-    );
-  });
-}
+//     initializePassport(
+//       passport,
+//       email => result.find(user => user.email === email),
+//       id => result.find(user => user.id === id)
+//     );
+//   });
+// }
 
 
-setInterval(updateUserListForPassport, 3000)
+// setInterval(updateUserListForPassport, 3000)
 
 
 
@@ -42,29 +40,64 @@ setInterval(updateUserListForPassport, 3000)
 // const users = [];
 
 // Declaring the PORT
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 
-// Specifing that we are using the EJS view Engine - Needs to be changed to Handlebars
-server.set('view-engine', 'ejs');
-server.use(express.urlencoded({ extended: false }));
-server.use(flash());
-server.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
-server.use(passport.initialize());
-server.use(passport.session());
-server.use(methodOverride('_method'));
-// Telling the server to use the API routes specified in the required routes.js File
+// // Specifing that we are using the EJS view Engine - Needs to be changed to Handlebars
+// server.set('view-engine', 'ejs');
+// server.use(express.urlencoded({ extended: true }));
+// server.use(express.json());
+// server.use(express.static("public"));
+
+// server.use(flash());
+// server.use(session({
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: false
+// }));
+// server.use(passport.initialize());
+// server.use(passport.session());
+// server.use(methodOverride('_method'));
+// // Telling the server to use the API routes specified in the required routes.js File
+// // Requiring our routes
+// require("./routes/html-routes.js")(server);
+// require("./routes/api-routes.js")(server);
+
+
+// // telling the server to start and listen for connections
+// db.sequelize.sync().then(function () {
+//   server.listen(PORT, function () {
+//     console.log("Server listening on: http://localhost:" + PORT);
+//   });
+// });
+
+
+// Requiring necessary npm packages
+var express = require("express");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
+
+// Setting up port and requiring models for syncing
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
+
+// Creating express app and configuring middleware needed for authentication
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Requiring our routes
-require("./routes/html-routes.js")(server);
-require("./routes/api-routes.js")(server);
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
-
-// telling the server to start and listen for connections
-db.sequelize.sync().then(function () {
-  server.listen(PORT, function () {
-    console.log("Server listening on: http://localhost:" + PORT);
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
